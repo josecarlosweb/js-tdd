23745271f9414099fca37918b35d2801ae190ebe
Carlos Carvalho <jcarloss.carvalho@gmail.com>
2021-06-01 07:35:55 -0300

    configure optimization webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'source-map',
   entry: {
     filename: path.resolve(__dirname, 'index.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js',
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
 };
